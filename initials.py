1ce6ae1b006013a7f7a2e267470a6b651aaaaa96
yemotiokorOdoi-Josephine <2425403528@live.gctu.edu.gh>
2026-05-30 13:49:20 +0000

    submit assigment

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
+#File:initials.py
+#Description:Print out my initials
+#Assignment Number: 2
+#
+#Name: Josephine Odoi Yemotiokor
+#Student ID:2425403528
+#Email:yemtiokorphina123@gmail.com/2425403528@live.gctu.edu.gh
+#Grader:Augustus Buckman
+#
+#On my honor, Josephine Odoi Yemotiokor, this programming assignment is my own work
+#and I have not provided this code to any other student
 
 def main():
-  pass #code goes here
-
-
-
+    #stylish my initials using the print function
+    print()
+    print("...JOY")
+    print()
+    print("...""JJJJJJJJJJJJ""........"  "OOOOOOOOOOOO""........"  "YYY......YYY"".....")
+    print("..."".........JJJ""........"  "OOOOOOOOOOOO""........"  ".YYY....YYY."".....")
+    print("..."".........JJJ""........"  "OOOO....OOOO""........"  "..YYY..YYY.."".....")
+    print("..."".........JJJ""........"  "OOOO....OOOO""........"  "...YYYYYY..."".....")
+    print("..."".........JJJ""........"  "OOOO....OOOO""........"  "....YYYY...."".....")
+    print("..."".........JJJ""........"  "OOOO....OOOO""........"  "....YYYY...."".....")
+    print("..."".........JJJ""........"  "OOOO....OOOO""........"  "....YYYY...."".....")
+    print("..."".........JJJ""........"  "OOOO....OOOO""........"  "....YYYY...."".....")
+    print("...""JJJJJJJJJJJJ""...**..."  "OOOOOOOOOOOO""...**..."  "....YYYY....""...**")
+    print("...""JJJJJJJJJJJJ""...**..."  "OOOOOOOOOOOO""...**..."  "....YYYY....""...**")
+    print()
 main()
